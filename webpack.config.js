03b74f534276c69bb566a8ccb4ba4837fdb854e7
Sam Chillcott <sam@samchillcott.com>
2020-08-20 09:42:46 +0100

    fixed export

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
-const path = require('path');
+const path = require("path");
 
-module.exports {
-    entry: './src/index.js',
-    output: {
-        filename: 'js/[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    watch: true,
-    resolve: {
-        extensions: [
-            ".js"
-        ]
-    }
-}
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "js/[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	watch: true,
+	resolve: {
+		extensions: [".js"],
+	},
+};
